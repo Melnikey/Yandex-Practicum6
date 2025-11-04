a7db1d80437663ea21580908eab0ad4f02e94ccf
Fedor Melnik <melnikey@bk.ru>
2025-11-04 17:27:43 +0300

    try1

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,14 @@
 package service
+import (
+	"strings"
+	"pkg/morse"
+)
+
+func Convert(x string) (y string, err error) {
+ prefix1 := "."
+ prefix2 := "-"
+ if strings.HasPrefix(x, prefix1) || strings.HasPrefix(x, prefix2) {
+	To
+ }
+	return y, nil
+} 
